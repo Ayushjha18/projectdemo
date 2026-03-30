9270d0d9666eec746ed8d1d3ce2208dbb06bbd17
Ayush jha <ayushjha5060@gmail.com>
2026-03-31 00:17:55 +0530

    Update Main.java
    odd even program

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,16 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner (System.in);
-        System.out.println("Enetr your age");
-        int age = sc.nextInt();
+        System.out.println("Enetr your number ");
+        int x = sc.nextInt();
 
-        if(age>= 18){
-            System.out.println("You are old");
-        }else if (age >=16){
-            System.out.println("minnor");
-        }  else {
-            System.out.println("wrong numer age shoud in positive number");
+        if(x % 2 = 0){
+            System.outt.println("number is even");
+        }else {
+            System.out.println("numer is odd");
+
         }
+        
 
     //System.out.println("sum = "+sum);
 
