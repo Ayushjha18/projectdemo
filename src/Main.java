11e6af342f6ef7a60fe1b9978981df395e0ea3d0
Ayushjha18 <ayushjha5060@gmail.com>
2026-03-31 00:10:00 +0530

    new addition

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,22 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enetr your age");
+        int age = sc.nextInt();
+
+        if(age>= 18){
+            System.out.println("You are old");
+        }else if (age >=16){
+            System.out.println("minnor");
+        }  else {
+            System.out.println("wrong numer age shoud in positive number");
+        }
+
+    //System.out.println("sum = "+sum);
+
+
 
     }
 }
